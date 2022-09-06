cdbda55d75eeffc75d3cb0cdf3c290453348035f
YaraHisham61 <yara.hisham.yh@gmail.com>
2022-09-06 15:39:20 +0200

    adding DIO registers

---
+++ On-demand Traffic/main.c
@@ -0,0 +1,18 @@
+/*
+ * On-demand Traffic.c
+ *
+ * Created: 9/6/2022 3:21:38 PM
+ * Author : YARA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ On-demand Traffic/types.h
@@ -0,0 +1,14 @@
+/*
+ * types.h
+ *
+ * Created: 9/6/2022 3:31:48 PM
+ *  Author: YARA
+ */ 
+
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef  unsigned char uint8_t;
+
+#endif /* TYPES_H_ */
